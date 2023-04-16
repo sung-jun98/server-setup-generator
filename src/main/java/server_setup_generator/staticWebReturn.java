0f901526d38fa7257ef5de36fe902b549e4a9507
김성준 <sjkim2243@gmail.com>
2023-04-16 20:33:56 +0900

    테스트파일 testClass.java 추가 및 2건
    매개변수를 통한 생성자를 통해서 웰컴페이지를 설정할 수 있도록 staticWebReturn.java를 수정하였고, 이제 프론트에서 HTML파일을 드래그앤드롭으로 다이어그램안에 놓은 다음 이를 AJAX를 통해 서버에서 받게 하면 OK

--- src/main/java/server_setup_generator/staticWebReturn.java
+++ src/main/java/server_setup_generator/staticWebReturn.java
@@ -1,9 +1,32 @@
 package server_setup_generator;
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(urlPatterns = "/", loadOnStartup = 1) // 웰컴 페이지 설정
+public class staticWebReturn extends HttpServlet {
+	private String pageContent;
+	
+	public staticWebReturn() {
+		this.pageContent = "<h1>Welcome to My Web Application!</h1>";
+	}
+	public staticWebReturn(String pageContent) {
+		this.pageContent = pageContent;
+		
+	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 웰컴 페이지의 동작 설정
+        response.getWriter().write(this.pageContent); // 응답 데이터 설정
+    }
+    
+    public String getPageContent() { //디버깅용 메서드
+    	return this.pageContent;
+    }
+}
+/*
 @WebServlet("/hello")
 public class staticWebReturn extends HttpServlet{
 	@Override
@@ -12,5 +35,5 @@ public class staticWebReturn extends HttpServlet{
 		System.out.println(request);
 		
 	}
-}
+}*/
 
---
+++ src/main/java/server_setup_generator/testClass.java
@@ -0,0 +1,12 @@
+package server_setup_generator;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		staticWebReturn test = new staticWebReturn("hello");
+		System.out.println(test.getPageContent());
+		
+	}
+
+}
